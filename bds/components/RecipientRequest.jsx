148ac3969977505dd99b08eaa58a83674e744a19
PYASVANTH <yasvanth.p2023it@sece.ac.in>
2025-01-31 09:32:49 +0530

    change

--- bds/components/RecipientRequest.jsx
+++ bds/components/RecipientRequest.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import axios from "axios";
-import '../src/styles/RecepientRequest.css';
+import '../src/components/recipientrequest.css';
 
 
 const RecipientRequest = () => {
